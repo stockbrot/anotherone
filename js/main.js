a260d2938c0cfa208d694afef99e34ea4a0e9af5
Tanis Webb <taniswebb@gmail.com>
2018-02-26 21:42:38 +0100

    'first'

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const electron = require('electron')
+const {
+    app,
+    BrowserWindow
+} = electron
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+    let {
+        width,
+        height
+    } = electron.screen.getPrimaryDisplay().workAreaSize
+
+    if (width <= 1700) {
+        width = 1700
+    }
+
+    mainWindow = new BrowserWindow({
+        width: width * .5,
+        height: height * .9,
+        frame: false
+    })
+
+    mainWindow.on('unresponsive', function () { })
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
---
+++ js/vue.js
@@ -0,0 +1,23 @@
+/*global Vue */
+/*eslint no-unused-vars: */
+/*eslint no-undef: */
+
+var fs = require('fs')
+
+/* Extra-Resource folder Location */
+let location = __dirname.substring(0, __dirname.length - 11)
+
+const vue = new Vue({
+    el: '#vue',
+    data: {
+
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+
+    }
+})
